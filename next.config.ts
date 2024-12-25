77460db01e05d3bf86d70f900ea75e3e9b0cc79b
thechamp84 <himanshusonkar84@gmail.com>
2024-12-26 00:09:21 +0530

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', 
+  // images: {
+  //   unoptimized: true, // Disable server-side image optimization for GitHub Pages
+  // },
+  basePath: '/portfolio',
 };
 
 export default nextConfig;
+
+
